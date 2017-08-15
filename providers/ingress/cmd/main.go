b607428227600722dd31e27ed9de8ff1e6ac61c9
Jim Zhang <jim.zoumo@gmail.com>
2017-08-15 16:40:34 +0800

    feat: add ingress controller sidecar

---
+++ providers/ingress/cmd/main.go
@@ -0,0 +1,150 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+	"os/signal"
+	"sort"
+	"syscall"
+	"time"
+
+	"github.com/caicloud/loadbalancer-controller/pkg/tprclient"
+	corenode "github.com/caicloud/loadbalancer-provider/core/pkg/node"
+	core "github.com/caicloud/loadbalancer-provider/core/provider"
+	"github.com/caicloud/loadbalancer-provider/providers/ingress/provider"
+	"github.com/caicloud/loadbalancer-provider/providers/ingress/version"
+	log "github.com/zoumo/logdog"
+	"gopkg.in/urfave/cli.v1"
+
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/util/wait"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/tools/clientcmd"
+)
+
+// Run ...
+func Run(opts *Options) error {
+	log.Info("Provider Build Information", log.Fields{
+		"release": version.RELEASE,
+		"commit":  version.COMMIT,
+		"repo":    version.REPO,
+	})
+
+	log.Info("Provider Running with", log.Fields{
+		"debug":     opts.Debug,
+		"kubconfig": opts.Kubeconfig,
+		"lb.ns":     opts.LoadBalancerNamespace,
+		"lb.name":   opts.LoadBalancerName,
+		"pod.name":  opts.PodName,
+		"pod.ns":    opts.PodNamespace,
+	})
+
+	if opts.Debug {
+		log.ApplyOptions(log.DebugLevel)
+	} else {
+		log.ApplyOptions(log.InfoLevel)
+	}
+
+	// build config
+	log.Infof("load kubeconfig from %s", opts.Kubeconfig)
+	config, err := clientcmd.BuildConfigFromFlags("", opts.Kubeconfig)
+	if err != nil {
+		log.Fatal("Create kubeconfig error", log.Fields{"err": err})
+		return err
+	}
+
+	// create clientset
+	clientset, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		log.Fatal("Create kubernetes client error", log.Fields{"err": err})
+		return err
+	}
+
+	// create tpr clientset
+	tprclientset, err := tprclient.NewForConfig(config)
+	if err != nil {
+		log.Fatal("Create tpr client error", log.Fields{"err": err})
+		return err
+	}
+
+	// get node ip
+	nodeIP, err := corenode.GetNodeIPForPod(clientset, opts.PodNamespace, opts.PodName)
+	if err != nil {
+		log.Fatal("Can not get node ip", log.Fields{"err": err})
+		return err
+	}
+
+	lb, err := tprclientset.NetworkingV1alpha1().LoadBalancers(opts.LoadBalancerNamespace).Get(opts.LoadBalancerName, metav1.GetOptions{})
+	if err != nil {
+		log.Fatal("Can not find loadbalancer resource", log.Fields{"lb.ns": opts.LoadBalancerNamespace, "lb.name": opts.LoadBalancerName})
+		return err
+	}
+
+	sidecar, err := provider.NewIngressSidecar(nodeIP, lb)
+	if err != nil {
+		return err
+	}
+
+	lp := core.NewLoadBalancerProvider(&core.Configuration{
+		KubeClient:            clientset,
+		TPRClient:             tprclientset,
+		Backend:               sidecar,
+		LoadBalancerName:      opts.LoadBalancerName,
+		LoadBalancerNamespace: opts.LoadBalancerNamespace,
+		TCPConfigMap:          lb.Status.ProxyStatus.TCPConfigMap,
+		UDPConfigMap:          lb.Status.ProxyStatus.UDPConfigMap,
+	})
+
+	// handle shutdown
+	go handleSigterm(lp)
+
+	lp.Start()
+
+	// never stop until sigterm processed
+	<-wait.NeverStop
+
+	return nil
+}
+
+func main() {
+	// fix for avoiding glog Noisy logs
+	flag.CommandLine.Parse([]string{})
+
+	app := cli.NewApp()
+	app.Name = "ingress sidecar"
+	app.Version = version.RELEASE
+	app.Compiled = time.Now()
+
+	// add flags to app
+	opts := NewOptions()
+	opts.AddFlags(app)
+
+	app.Action = func(c *cli.Context) error {
+		if err := Run(opts); err != nil {
+			msg := fmt.Sprintf("running loadbalancer controller failed, with err: %v\n", err)
+			return cli.NewExitError(msg, 1)
+		}
+		return nil
+	}
+
+	sort.Sort(cli.FlagsByName(app.Flags))
+
+	app.Run(os.Args)
+}
+
+func handleSigterm(p *core.GenericProvider) {
+	signalChan := make(chan os.Signal, 1)
+	signal.Notify(signalChan, syscall.SIGTERM)
+	<-signalChan
+	log.Infof("Received SIGTERM, shutting down")
+
+	exitCode := 0
+	if err := p.Stop(); err != nil {
+		log.Infof("Error during shutdown %v", err)
+		exitCode = 1
+	}
+
+	log.Infof("Exiting with %v", exitCode)
+	os.Exit(exitCode)
+}
---
+++ providers/ingress/provider/sidecar.go
@@ -0,0 +1,253 @@
+package provider
+
+import (
+	"net"
+	"reflect"
+	"strings"
+
+	netv1alpha1 "github.com/caicloud/loadbalancer-controller/pkg/apis/networking/v1alpha1"
+	"github.com/caicloud/loadbalancer-controller/pkg/util/validation"
+	corenet "github.com/caicloud/loadbalancer-provider/core/pkg/net"
+	"github.com/caicloud/loadbalancer-provider/core/pkg/sysctl"
+	core "github.com/caicloud/loadbalancer-provider/core/provider"
+	log "github.com/zoumo/logdog"
+
+	"k8s.io/ingress/controllers/nginx/pkg/version"
+	utildbus "k8s.io/kubernetes/pkg/util/dbus"
+	k8sexec "k8s.io/kubernetes/pkg/util/exec"
+	"k8s.io/kubernetes/pkg/util/iptables"
+)
+
+const (
+	tableRaw      = "raw"
+	iptablesChain = "INGRESS-CONTROLLER"
+)
+
+var (
+	sysctlAdjustments = map[string]string{
+		// about time wait connection
+		// https://vincent.bernat.im/en/blog/2014-tcp-time-wait-state-linux
+		// deprecated: allow to reuse TIME-WAIT sockets for new connections when it is safe from protocol viewpoint
+		// "net.ipv4.tcp_tw_reuse": "1",
+		// deprecated: enable fast recycling of TIME-WAIT sockets
+		// "net.ipv4.tcp_tw_recycle": "1",
+
+		// about tcp keepalive
+		// set tcp keepalive timeout time
+		"net.ipv4.tcp_keepalive_time": "1800",
+		// set tcp keepalive probe interval
+		"net.ipv4.tcp_keepalive_intvl": "30",
+		// set tcp keepalive probe times
+		"net.ipv4.tcp_keepalive_probes": "3",
+
+		// reduse time wait buckets
+		"net.ipv4.tcp_max_tw_buckets": "6000",
+		// expand local port range
+		"net.ipv4.ip_local_port_range": "10240 65000",
+		// reduse time to hold socket in state FIN-WAIT-2
+		"net.ipv4.tcp_fin_timeout": "30",
+		// increase the maximum length of the queue for incomplete sockets
+		"net.ipv4.tcp_max_syn_backlog": "8192",
+
+		// increase the queue length for completely established sockets
+		"net.core.somaxconn": "2048",
+		// expand number of unprocessed input packets before kernel starts dropping them
+		"net.core.netdev_max_backlog": "262144",
+	}
+)
+var _ core.Provider = &IngressSidecar{}
+
+// IngressSidecar ...
+type IngressSidecar struct {
+	nodeInfo      *corenet.Interface
+	storeLister   core.StoreLister
+	ipt           iptables.Interface
+	sysctlDefault map[string]string
+	tcpPorts      []string
+	udpPorts      []string
+}
+
+// NewIngressSidecar creates a new ingress sidecar
+func NewIngressSidecar(nodeIP net.IP, lb *netv1alpha1.LoadBalancer) (*IngressSidecar, error) {
+	nodeInfo, err := corenet.InterfaceByIP(nodeIP.String())
+	if err != nil {
+		log.Error("get node info err", log.Fields{"err": err})
+		return nil, err
+	}
+	execer := k8sexec.New()
+	dbus := utildbus.New()
+	iptInterface := iptables.New(execer, dbus, iptables.ProtocolIpv4)
+
+	sidecar := &IngressSidecar{
+		nodeInfo:      nodeInfo,
+		sysctlDefault: make(map[string]string),
+		ipt:           iptInterface,
+	}
+
+	return sidecar, nil
+}
+
+// OnUpdate ...
+func (p *IngressSidecar) OnUpdate(lb *netv1alpha1.LoadBalancer) error {
+	// p.reloadRateLimiter.Accept()
+
+	if err := validation.ValidateLoadBalancer(lb); err != nil {
+		log.Error("invalid loadbalancer", log.Fields{"err": err})
+		return nil
+	}
+
+	// filtered
+	if lb.Spec.Type != netv1alpha1.LoadBalancerTypeExternal || lb.Spec.Providers.Ipvsdr == nil {
+		return nil
+	}
+
+	tcpcm, err := p.storeLister.ConfigMap.ConfigMaps(lb.Namespace).Get(lb.Status.ProxyStatus.TCPConfigMap)
+	if err != nil {
+		log.Error("can not find tcp configmap for loadbalancer")
+		return err
+	}
+	udpcm, err := p.storeLister.ConfigMap.ConfigMaps(lb.Namespace).Get(lb.Status.ProxyStatus.UDPConfigMap)
+	if err != nil {
+		log.Error("can not find udp configmap for loadbalancer")
+		return err
+	}
+
+	tcpPorts, udpPorts := core.GetExportedPorts(tcpcm, udpcm)
+
+	if reflect.DeepEqual(p.tcpPorts, tcpPorts) && reflect.DeepEqual(p.udpPorts, udpPorts) {
+		// no change
+		return nil
+	}
+
+	log.Info("Updating config")
+
+	p.tcpPorts = tcpPorts
+	p.udpPorts = udpPorts
+
+	p.ensureIptablesNotrack(tcpPorts, udpPorts)
+
+	return nil
+}
+
+// Start ...
+func (p *IngressSidecar) Start() {
+	log.Info("Startting ingress sidecar provider")
+
+	p.changeSysctl()
+	p.ensureChain()
+	return
+}
+
+// WaitForStart ...
+func (p *IngressSidecar) WaitForStart() bool {
+	return true
+}
+
+// Stop ...
+func (p *IngressSidecar) Stop() error {
+	log.Info("Shutting down ingress sidecar provider")
+
+	err := p.resetSysctl()
+	if err != nil {
+		log.Error("reset sysctl error", log.Fields{"err": err})
+	}
+
+	p.deleteChain()
+
+	return nil
+}
+
+// Info ...
+func (p *IngressSidecar) Info() core.Info {
+	return core.Info{
+		Name:       "ingress-sidecar",
+		Release:    version.RELEASE,
+		Build:      version.COMMIT,
+		Repository: version.REPO,
+	}
+}
+
+// SetListers sets the configured store listers in the generic ingress controller
+func (p *IngressSidecar) SetListers(lister core.StoreLister) {
+	p.storeLister = lister
+}
+
+// changeSysctl changes the required network setting in /proc to get
+// keepalived working in the local system.
+func (p *IngressSidecar) changeSysctl() error {
+	var err error
+	p.sysctlDefault, err = sysctl.BulkModify(sysctlAdjustments)
+	if err != nil {
+		log.Error("error change sysctl", log.Fields{"err": err})
+		return err
+	}
+	return nil
+}
+
+// resetSysctl resets the network setting
+func (p *IngressSidecar) resetSysctl() error {
+	log.Info("reset sysctl to original value", log.Fields{"defaults": p.sysctlDefault})
+	_, err := sysctl.BulkModify(p.sysctlDefault)
+	return err
+}
+
+func (p *IngressSidecar) ensureChain() {
+	// create chain
+	ae, err := p.ipt.EnsureChain(tableRaw, iptables.Chain(iptablesChain))
+	if err != nil {
+		log.Fatalf("unexpected error: %v", err)
+	}
+	if ae {
+		log.Infof("chain %v already existed", iptablesChain)
+	}
+
+	// add rule to let all traffic jump to our chain
+	p.ipt.EnsureRule(iptables.Append, tableRaw, iptables.ChainPrerouting, "-j", iptablesChain)
+}
+
+func (p *IngressSidecar) flushChain() {
+	log.Info("flush iptables rules", log.Fields{"table": tableRaw, "chain": iptablesChain})
+	p.ipt.FlushChain(tableRaw, iptables.Chain(iptablesChain))
+}
+
+func (p *IngressSidecar) deleteChain() {
+	// flush chain
+	p.flushChain()
+	// delete jump rule
+	p.ipt.DeleteRule(tableRaw, iptables.ChainPrerouting, "-j", iptablesChain)
+	// delete chain
+	p.ipt.DeleteChain(tableRaw, iptablesChain)
+}
+
+func (p *IngressSidecar) setIptablesNotrack(protocol string, ports []string) (bool, error) {
+	args := make([]string, 0)
+	args = append(args, "-i", p.nodeInfo.Name, "-p", protocol)
+
+	if len(ports) > 0 {
+		args = append(args, "-m", "multiport", "--dports", strings.Join(ports, ","))
+	}
+	args = append(args, "-j", "NOTRACK")
+
+	return p.ipt.EnsureRule(iptables.Append, tableRaw, iptablesChain, args...)
+}
+
+func (p *IngressSidecar) ensureIptablesNotrack(tcpPorts, udpPorts []string) {
+	log.Info("ensure iptables rules")
+
+	// flush all rules
+	p.flushChain()
+
+	if len(tcpPorts) > 0 {
+		_, err := p.setIptablesNotrack("tcp", tcpPorts)
+		if err != nil {
+			log.Error("error ensure iptables tcp rule for", log.Fields{"tcpPorts": tcpPorts})
+		}
+	}
+	if len(udpPorts) > 0 {
+		_, err := p.setIptablesNotrack("udp", udpPorts)
+		if err != nil {
+			log.Error("error ensure iptables udp rule for", log.Fields{"udpPorts": udpPorts})
+		}
+	}
+
+}
