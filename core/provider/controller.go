3525ccee88e1466cef76ba4e809c072e2c60ee90
Jim Zhang <jim.zhang@caicloud.io>
2018-04-19 20:04:26 +0800

    chore(k8s): update kubernetes api to 1.10

--- core/provider/controller.go
+++ core/provider/controller.go
@@ -30,7 +30,7 @@ import (
 
 	"k8s.io/apimachinery/pkg/api/errors"
 	utilruntime "k8s.io/apimachinery/pkg/util/runtime"
-	"k8s.io/client-go/pkg/api/v1"
+	"k8s.io/api/core/v1"
 	"k8s.io/client-go/tools/cache"
 )
 
--- core/provider/ports.go
+++ core/provider/ports.go
@@ -16,7 +16,7 @@ limitations under the License.
 
 package provider
 
-import "k8s.io/client-go/pkg/api/v1"
+import "k8s.io/api/core/v1"
 
 var (
 	// ReservedTCPPorts represents the reserved tcp ports
